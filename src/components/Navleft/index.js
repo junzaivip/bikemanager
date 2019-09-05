53565ac052a9523d100de72065c3e43269b95905
junzaivip <goxuexi@qq.com>
2019-09-05 11:04:33 +0800

    菜单的渲染 和 添加资源文件

--- src/components/Navleft/index.js
+++ src/components/Navleft/index.js
@@ -1,12 +1,60 @@
+import menuConfig from './../../config/menuConfig'
+import './index.less'
+
 import React from 'react'
-
-export default class Navleft extends React.Component{
-    render(){
+import {Menu, Icon} from 'antd';
 
 
-        return(
-            <div style={{backgroundColor:'red'}}>
-                Navleft
+
+const {SubMenu} = Menu;
+
+
+
+
+export default class Navleft extends React.Component {
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(menuConfig);
+        this.setState({
+            menuTreeNode
+        })
+
+    }
+
+    // 菜单渲染 递归
+    renderMenu=(data)=>{
+
+        return data.map((item)=>{
+            if(item.children){
+                return(
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>
+                {item.title}
+            </Menu.Item>
+
+
+        });
+
+    }
+
+
+    render() {
+
+
+        return (
+            <div>
+                <div className='logo'>
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>like mage</h1>
+
+                </div>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
+
+                </Menu>
             </div>
         )
     }
---
+++ src/resource/menuConfig.js
@@ -0,0 +1,9 @@
+const menuList = [
+    {
+        title:'首页',
+        key:'/admin/home'
+    }
+
+]
+
+export default menuList;
