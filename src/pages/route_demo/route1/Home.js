6811d2b6133f4266b525db608793e2fcbf923708
junzaivip <goxuexi@qq.com>
2019-09-07 14:47:13 +0800

    React touter 4 demo添加

---
+++ src/pages/route_demo/route1/Home.js
@@ -0,0 +1,43 @@
+import React from  'react'
+import {HashRouter, Route, Link,Switch} from 'react-router-dom'
+import About from './About'
+import Main from './Main'
+import Topic from './Topic'
+
+export default class Home extends React.Component{
+
+
+    render(){
+
+        return(
+           <HashRouter>
+               <div>
+                   <ul>
+                       <li>
+                           <Link to="/" > Home</Link>
+                       </li>
+                       <li>
+                           <Link to="/about">About</Link>
+                       </li>
+                       <li>
+                           <Link to="/topics">Topic</Link>
+                       </li>
+                       <li>
+                           <Link to="/main">Main</Link>
+                       </li>
+
+                   </ul>
+
+                   <hr/>
+                   <Switch>
+                       <Route path="/" exact={true} component={Main}></Route>
+                       <Route path="/about" component={About}></Route>
+                       <Route path="/topics" component={Topic}></Route>
+                       <Route path="/main" component={Main}></Route>
+                   </Switch>
+               </div>
+
+           </HashRouter>
+        )
+    }
+}
